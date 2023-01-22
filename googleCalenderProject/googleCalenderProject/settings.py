f1d25e90f3c91c55c4382f98053a531c262244af
Pushpander Singh Tanwar <110348583+codeblooded1001000@users.noreply.github.com>
2023-01-22 14:19:26 +0530

    Updated settings.py

--- googleCalenderProject/googleCalenderProject/settings.py
+++ googleCalenderProject/googleCalenderProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o@t-i*g)jocji(atv-170=ffmvl(=1%l(sf=vb(=x7acg)++&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
